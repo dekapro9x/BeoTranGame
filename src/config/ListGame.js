36066af74c0da7147624704853e1914b25bfc8ee
Trần Xuân Nam <30828722+dekapro9x@users.noreply.github.com>
2021-12-22 17:51:31 +0700

    Update list menu

--- src/config/ListGame.js
+++
@@ -1,6 +0,0 @@
-var ListGame = {
-  menu: [
-    { id: 1, name: "Cờ Ca Rô" },
-    { id: 2, name: "Số 7 may mắn!" },
-  ],
-};
---
+++ src/config/MenuGame.js
@@ -0,0 +1,4 @@
+var ListMenuGame = [
+  { id: 1, name: "Cờ Ca Rô" },
+  { id: 2, name: "Số 7 may mắn!" },
+];
